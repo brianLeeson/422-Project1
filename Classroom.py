c2dba830b592f302a7687f9d1a6e2f7fc4cd7964
brian leeson <brianeleeson@gmail.com>
2017-04-06 18:22:15 -0700

    skeleton for Classroom Class. more outline work to do with group.

---
+++ Classroom.py
@@ -0,0 +1,10 @@
+class Classroom:
+
+	def __init__(self):
+		self._studentList = []
+		self._groupList = []
+		# TODO Is this how we want our weight to look? We break them out into individual attributes
+		self.weights = []
+
+		# TODO Getters and setters
+
