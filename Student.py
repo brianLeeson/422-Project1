96e52d86c7579125dc50049bd372210bf2192c0a
Jamie Zimmerman <jamiez@uoregon.edu>
2017-04-10 17:25:15 -0700

    added code experience attributes

--- Student.py
+++ Student.py
@@ -1,5 +1,5 @@
 """
-Author(s): Brian Leeson
+Author(s): Brian Leeson + Jamie Zimmerman
 
 """
 
@@ -13,10 +13,17 @@ class Student:
 	# There might be some aliasing issues we could nip in the bud if we chose tuples
 
 	def __init__(self, name, email, criteria):
-		self._UID = 0  # TODO implement UID as a UUID? *Can't* be student ID. FERPA
-		self._name = name
-		self._email = email
-		self._criteria = criteria
+		self.UID = 0  # TODO implement UID as a UUID? *Can't* be student ID. FERPA
+		self.name = name
+		self.email = email
+		self.criteria = criteria
+		
+		self.overallExperience = 0 #overall experience score - how many upper div CS classes completed
+		self.codeExperience = {'Python': 0,
+					'Java': 0,
+					'JavaScript': 0,
+					'C': 0,
+					'C++': 0}
 
 	def __str__(self):
 		return self.getName() + " " + self.getUID()
@@ -25,18 +32,36 @@ class Student:
 		return self.getUID() == other.getUID()
 
 	def getName(self):
-		return self._name
+		return self.name
 
 	def setName(self, name):
-		self._name = name
+		self.name = name
 		return None
 
 	def getUID(self):
-		return self. _UID
+		return self.UID
 
 	def setUID(self, UID):
-		self._UID = UID
+		self.UID = UID
+		return None
+	
+	def getOverallExperience(self):
+		return self.overallExperience
+	def setOverallExperience(self, exp):
+		self.overallExperience = exp
 		return None
 
+	def getCodeExperience(self):
+		return self.codeExperience
+	def setCodeExperience(self, tool, capability):
+		self.codeExperience[tool] = capability
 	# TODO figure out how criteria is represented
 
+
+#-----------------------------------Sandbox Area -------------------------------------------------#
+if __name__ == '__main__':
+	student = Student('Brian', 'brian@brian.com', 5)
+	student.setCodeExperience('Python', 8)
+	print(student.getCodeExperience())
+	student.setOverallExperience(4)
+	print('{} is this qualified: {}'.format(student.getName(), student.getOverallExperience()))
