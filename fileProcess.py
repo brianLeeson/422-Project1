de5857f21ee6aaca13e698db87fb5bdeceb2c348
Jamie Zimmerman <jamiez@uoregon.edu>
2017-04-12 08:17:28 -0700

    began processing csv info - riddled with errors

--- fileProcess.py
+++ fileProcess.py
@@ -4,7 +4,7 @@ Author(s): Brian Leeson
 """
 
 import Student
-
+import csv
 def timestamp(meeting_time):
 	'''
 	input -> string, representation of a datetime meeting time
@@ -23,14 +23,33 @@ def process(fileName):
 	:param fileName: string of the file name in the local directory
 	:return: a list (?) of student instances
 	"""
-	with open(fileName, "r") as csvFile:
-		studentList = []
-		for line in csvFile:
-			# TODO parse line. we need to define what our google forms will be
+	studentList = []
+	with open(fileName, "r") as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in csvfile:
+			print(row['Student Name'])
+			name = row['Student Name']
+			pyth = row['Python experience']
+			java = row['Java experience']
+			js = row['Javascript experience']
+			c = row['C experience']
+			cpp = row['C++ experience']
+			php = row['PHP experience']
+			html = row['HTML experience']
+			sql = row['SQL experience']
+			bash = row['Bash/Unix experience']
 
+			mon =  row['Monday']
+			tues =  row['Tuesday']
+			wed = row['Wednesday']
+			thurs = row['Thursday']
+			fri = row['Friday']
+			     
 			student = Student.Student("FAKE NAME", ["FAKE CRITERIA"])
 			studentList.append(student)
 
 	return studentList
 
-
+#----------------------Sandbox Area--------------------------------------
+if __name__=='__main__':
+	process('example_survey_answer.csv')
