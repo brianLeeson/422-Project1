017cec95421712f3329838dcbd9740f1456e8107
brian leeson <brianeleeson@gmail.com>
2017-04-16 21:35:59 -0700

    no functionality changes. made PEP8 style guidelines stop yelling at me

--- fileProcess.py
+++ fileProcess.py
@@ -3,23 +3,26 @@ Author(s): Jamie Zimmerman
 
 """
 
-from Student import Student #from module import class
+from Student import Student  # from module import class
 import csv
+
+
 def timestamp(meeting_time):
-	'''
+	"""
 	input -> string, representation of a datetime meeting time
 	output -> datetime object
-	'''
-	#TODO determine if we need this function at all
+	"""
+	# TODO determine if we need this function at all
 	return
 
+
 def process(fileName):
-	'''
+	"""
 	input -> string of a csv file
 	reads the file using csv dictionary reader
 	takes the information from each row (which represents a student)
 	and creates a student object
-	'''
+	"""
 	studentList = []
 	with open(fileName, "r") as csvfile:
 		reader = csv.DictReader(csvfile)
@@ -28,7 +31,7 @@ def process(fileName):
 			name = row['Student Name']
 			pyth = row['Python experience']
 			java = row['Java experience']
-			js = row['Javascript experience']
+			js = row['Javascript experience']  # TODO: set js experience
 			c = row['C experience']
 			cpp = row['C++ experience']
 			php = row['PHP experience']
@@ -36,17 +39,18 @@ def process(fileName):
 			sql = row['SQL experience']
 			bash = row['Bash/Unix experience']
 
-			mon =  row['Monday']
-			tues =  row['Tuesday']
+			mon = row['Monday']
+			tues = row['Tuesday']
 			wed = row['Wednesday']
 			thurs = row['Thursday']
 			fri = row['Friday']
 			
-			requests = row["Desired Teammates @uoregon.edu emails (separated by ';')"]
+			requests = row["Desired Teammates @uoregon.edu emails (separated by ';')"]  # TODO: store requests
 
 			student = Student(name, email)
 			student.setCodeExperience('Python', pyth)
 			student.setCodeExperience('Java', java)
+			# student.setCodeExperience('Js', js) # I think this how you want it.
 			student.setCodeExperience('C', c)
 			student.setCodeExperience('C++', cpp)
 			student.setCodeExperience('PHP', php)
@@ -60,12 +64,12 @@ def process(fileName):
 			student.setAvailability('Thursday', thurs)
 			student.setAvailability('Friday', fri)
 
-			#TODO figure out how to create student objects from request list - these need to be added to the students teammate list
+			# TODO figure out how to create student objects from request list - these need to be added to the students teammate list
 			studentList.append(student)
 
 	return studentList
 
-#----------------------Sandbox Area--------------------------------------
-if __name__=='__main__':
+# ----------------------Sandbox Area--------------------------------------
+if __name__ == '__main__':
 	for guy in process('example_survey_answer.csv'):
 		print(guy)
