efd5bce9067db33d7f4aa9318a5188f61da61107
brian leeson <brianeleeson@gmail.com>
2017-04-16 19:39:04 -0700

    MVP GUI almost working. need jamie's updated code

--- groupApp.py
+++ groupApp.py
@@ -4,24 +4,89 @@ Author(s): Brian Leeson
 """
 
 import Classroom
-import tkinter
-import fileProcess
+import tkinter as tk
+from tkinter import filedialog
+import fileProcess as fp
 
-app = tkinter.Tk()
-classroom = Classroom.Classroom()
 
 # TODO: Should we consider a manual entry? what if google forms doesn't work?
 
 # create window
 # window has:
-# import csv button - auto processes.
-# weight labels and sliders
-# students per group slider
+# import csv button - should auto processes.
 # sort button
-# "screen" to view results
-# export to txt button
+# export to csv button
 
+# WISHLIST - in order
+# students per group slider
+# weight labels and sliders
+# "screen" to view results
+
+# Classroom instance stores data
+classroom = Classroom.Classroom()
+
+
+# DEFINE FUNCTIONS
+def importCB():
+	"""
+	called when import button clicked
+	function prompts user to select path to csv and
+	process csv
+	"""
+	# prompt user for path
+	path = filedialog.askopenfilename()
+
+	# files process
+	studentCollection = fp.process(path)
+
+	# assign to classroom class
+	classroom.setStudentList(studentCollection)
+
+	print("imported")
+
+	return None
+
+
+def sortCB():
+	"""
+	Function calls sort on classroom class.
+	"""
+	classroom.sortIntoTeams()
+
+	print("sorted")
+
+	return None
+
+
+def exportCB():
+	"""
+	Function exports csv of sort teams to cwd
+	"""
+	# TODO: Need function in file process to call
+
+	print("exported")
+
+	return None
+
+
+# create app
+app = tk.Tk()
+
+mainFrame = tk.Frame(app)
+mainFrame.grid(row=0, column=0)
+
+importButton = tk.Button(mainFrame, text="import", command=importCB)
+importButton.grid(row=0, column=0)
+
+sortButton = tk.Button(mainFrame, text="sort", command=sortCB)
+sortButton.grid(row=1, column=0)
+
+exportButton = tk.Button(mainFrame, text="export", command=exportCB)
+exportButton.grid(row=2, column=0)
+
+canvas = tk.Canvas(app, width=500, height=500, borderwidth=10, bg="white")
+canvas.grid(row=0, column=1)
 
 print("I ran a test app")
-
+# runs the app
 app.mainloop()
