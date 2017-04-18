a2d1e338c0239a823250ca9ace359f5c5c695f46
Jamie Zimmerman <jamiez@uoregon.edu>
2017-04-17 20:02:42 -0700

    added documentation comments

--- fileProcess.py
+++ fileProcess.py
@@ -4,6 +4,7 @@ Author(s): Jamie Zimmerman
 """
 
 from Student import Student  # from module import class
+from Team import Team
 import csv
 
 
@@ -20,7 +21,7 @@ def timestamp(meeting_time):
 
 def process(fileName):
 	"""
-	input -> string of a csv file
+	input -> string of a csv file, ex: '422_Project_Template.csv'
 	reads the file using csv dictionary reader
 	takes the information from each row (which represents a student)
 	and creates a student object
@@ -28,19 +29,10 @@ def process(fileName):
 	studentList = []
 	with open(fileName, "r") as csvfile:
 		reader = csv.DictReader(csvfile)
+		languages = ['Python', 'Java', 'Javascript', 'C', 'C++', 'PHP', 'HTML', 'SQL', 'Bash/Unix']
+		survey_headers = ['Python experience', 'Java experience', 'Javascript experience', 'C experience', 'C++ experience', 'PHP experience', 'HTML experience', 'SQL experience', 'Bash/Unix experience']
+		
 		for row in reader:
-			duckID = row['Your DuckID']
-			name = row['Student Name']
-			pyth = row['Python experience']
-			java = row['Java experience']
-			js = row['Javascript experience']
-			c = row['C experience']
-			cpp = row['C++ experience']
-			php = row['PHP experience']
-			html = row['HTML experience']
-			sql = row['SQL experience']
-			bash = row['Bash/Unix experience']
-
 			mon = row['Monday']
 			tues = row['Tuesday']
 			wed = row['Wednesday']
@@ -49,17 +41,10 @@ def process(fileName):
 			
 			requests = row["Desired Teammates DuckIDs (separated by ';')"]
 
-			student = Student(name, duckID)
-			student.setCodeExperience('Python', pyth)
-			student.setCodeExperience('Java', java)
-			student.setCodeExperience('Javascript', js)
-			student.setCodeExperience('C', c)
-			student.setCodeExperience('C++', cpp)
-			student.setCodeExperience('PHP', php)
-			student.setCodeExperience('HTML', html)
-			student.setCodeExperience('SQL', sql)
-			student.setCodeExperience('Bash/Unix', bash)
-			
+			student = Student(row['Student Name'], row['Your DuckID'])
+			for i in range(7): #TODO more elegant design
+				student.setCodeExperience(languages[i], int(row[survey_headers[i]]))
+
 			student.setAvailability(create_time_chart([mon, tues, wed, thurs, fri]))
 
 			# TODO figure out how to create student objects from request list - these need to be added to the students teammate list
@@ -75,11 +60,12 @@ def create_time_chart(day_availability_list):
 		the first item in the list is Monday, then Tuesday, etc... (the function zips this with in-order days so that the data is not scrambled)
 		within each 'day' is a semicolon separated list of available times slots
 	this function parses that availability into a binary representation
-	output - dictionary
+	output - dictionary - entire dictionary representing student's availability
 		key: days (Monday, Tuesday, etc.)
 		value: a list of 0's and 1's, which indicates which times slots are open for that student
 		ex: "Monday":[0, 1, 1, 0, 0] means that the student is available on Monday from 12:00-2:00& 2:00-4:00
 		the list should only be 5 spaces long, representing 4 time slots(10-12, 12-2, 2-4, 4-6) and None, meaning no time available
+		example usage: student.setAvailability(create_time_chart(day_availability_list))
 	'''
 	days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 	pre_chart = dict(zip(days, day_availability_list))
@@ -106,7 +92,9 @@ def create_time_chart(day_availability_list):
 def export(decided_teams):
 	"""
 	THIS FUNCTION ASSUMES THAT THE INPUT IS A LIST OF TEAM OBJECTS***
+	input-> list of Team objects
 	function writes teams as a csv to cwd
+	output-> None, writing the file is a side effect
 	"""
 	with open('team_decisions.csv', 'w') as csvfile:
 		fieldnames = ['Team Number', 'Student 1', 'Student 2', 'Student 3']
@@ -123,13 +111,14 @@ def export(decided_teams):
 
 # ----------------------Sandbox Area--------------------------------------
 if __name__ == '__main__':
-	'''for guy in process('422_Project1_Template.csv'):
+	#THIS CODE NEVER NEEDS TO BE SAVED, JUST A WORKSPACE AREA
+	for guy in process('422_Project1_Template.csv'):
 		print(guy)
 		print(guy.getAvailability())
+		print(guy.getCodeExperience())
 	b_chart = ["10:00-12:00;2:00-4:00", "2:00-4:00;4:00-6:00", "10:00-12:00;4:00-6:00", "None", "10:00-12:00;2:00-4:00"]
-<<<<<<< HEAD
-=======
-	'''
-	decided = process('422_Project1_Template.csv')
->>>>>>> a2a41240b4f932ed90ee8f2a6e483b2d66344fce
 	
+	decided = process('422_Project1_Template.csv')
+	team = Team(4)
+	team.setMemberList(decided)
+	export([team])
