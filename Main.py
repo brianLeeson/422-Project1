58a1a2c9b8667d85986fb537d00124d58faa8692
amiecorso <acorso@cs.uoregon.edu>
2017-04-22 13:22:40 -0700

    Committing changes after our Saturday meeting.  Mostly notes to Main.

--- Main.py
+++ Main.py
@@ -29,39 +29,20 @@ Add the logic to deal with unassigned students (groups not divisible by 3)
 
 - create some very small test cases, and intentionally unviable test cases
 
-
-
-Handling the case of a student(s) who don't appear on ANY teams... Or is this already taken care of?
+Handling the case of a student(s) who aren't viable on ANY teams... Or is this already taken care of?
 
 That weird bug where we lost a team......
 
 fix direct attribute reference with usage of getters and setters
-OR IS RALEIGH RIGHT AND THIS IS STUPID?
-
-make sure student.teammates list gets populated once sort is over
-OR DO WE EVEN NEED THIS?
-
-TEST cases
-
+DO IT
 
 Algorithm Wishlist:
-- request consideration
+- request consideration (up quality score) (student.requests)
 - useage of weights for sorting criteria
-
+- fix so that teamsize is a real parameter (itertools?)
 Clean up printing for better discussion?
 
-
-FOR MY COMRADES:
-- should export wipe the original CSV file?  Because right now it's appending to it
-- should multiple exports generate a new file?
-- what other data can we report in the CSV?
-- seems that the export isn't including 4th members
-- what's going on with that error message printing around the language dictionaries?
-
 IDEAS:
-if we fail to accommodate every student, we repeat the process starting with a different FIRST team assignment?
-Everything else stays the same... it just creates more opportunity for new seeds?
-
 What if we start by grabbing teams that have closer-to-average quality scores?
 so that we don't make any unbreakable teams by the random swap method....
 
@@ -70,5 +51,34 @@ Student Class:
 * why isn't getName working??
 
 How do I get the __str__ override to print a prettier version? (for the Team class, for example)
-"""
 
+
+BRIAN:
+- teamSize parameter (keep/discard?)
+- virtual environment?
+- documentation
+
+
+JAMIE:
+- testing on multiple platforms
+- unit tests?
+- class so big that it breaks
+- class with various multiples of teamsize (/not multiples)
+- classes that CAN'T form teams
+- classes that are smaller than teamsize
+- does CSV for sure overwrite? (not append)
+-
+- what other data can we report in the CSV?
+    for example:
+        overlapping languages
+        overlapped schedule (this would need be generated)
+        quality score
+
+- seems that the export isn't including team+1th members
+
+AMIE:
+-  algorithm notes above
+- comments/docstrings
+- fix what Jamie manages to break
+
+"""
